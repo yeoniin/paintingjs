b88272c14a025b2c8acbcd19580ad024ef678bf7
yeoniin <68108022+yeoniin@users.noreply.github.com>
2021-01-08 01:28:00 +0900

    PaintJS
    PaintJS made with VanilaJS

---
+++ app.js
@@ -0,0 +1,109 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 500;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white"; //default
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.strokeStyle="INITIAL_COLOR"; //default
+ctx.fillStyle = "INITIAL_COLOR";
+ctx.lineWidth = 2.5; //default
+//ctx.fillStyle = "green";
+//ctx.fillRect(50, 20, 100, 49);
+
+let painting = false;
+let filling = false;
+
+function stopPainting(){
+  painting = false;
+}
+
+function startPainting(){
+  painting = true;
+}
+
+function onMouseMove(event){
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if(!painting){
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+  }else{
+    ctx.lineTo(x,y);
+    ctx.stroke();
+  }
+}
+
+function handleColorClick(event){
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = ctx.strokeStyle;
+}
+
+function handleRangeChange(event){
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick(){
+  if(filling === true){
+    filling = false;
+    mode.innerText = "Fill";
+  }else{
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+function handleCanvasClick(){
+  if(filling){
+  ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+
+function handleCM(event){
+  event.preventDefault();
+}
+
+function handleSaveClick(){
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS[export]";
+  link.click();
+
+
+}
+
+if(canvas){
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
+}
+
+Array.from(colors).forEach(color =>
+  color.addEventListener("click",handleColorClick)
+); // 'Array from'은 object로부터 array를 만든다
+
+if(range){
+  range.addEventListener("input", handleRangeChange);
+}
+
+if(mode){
+  mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn){
+  saveBtn.addEventListener("click", handleSaveClick);
+}
